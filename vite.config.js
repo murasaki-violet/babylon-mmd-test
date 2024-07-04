bebc32e285ce10841dcebb207a19d8ec66846f39
murasaki_violet <tobiratory.murasaki@gmail.com>
2024-07-05 00:54:01 +0900

    vite.config.js変更

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-export default {
-    base: '',
-    build: {
-      outDir: 'dist'
-    }
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/your-repo-name/', // GitHub Pages のリポジトリ名に合わせて設定
+  build: {
+    outDir: 'dist'
   }
+});
